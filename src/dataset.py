f27ee874c4b9475391916a1768fda89b3243aa06
Arthur Sabirov <ar_sabirov@MacBook-Pro.local>
2020-06-25 15:33:06 +0300

    move dataset py

---
+++ src/dataset.py
@@ -0,0 +1,59 @@
+from torch.utils.data import Dataset
+import torch
+import pandas as pd
+from typing import Optional
+
+class CompanyDataset(Dataset):
+    def __init__(self,
+                 data_path: str,
+                 transform = None,
+                 nrows: Optional[int] = None):
+        self.df = pd.read_csv(data_path, sep='\t', index_col=0, nrows=nrows)
+        self.transform = transform
+        self.groups = self.df['ru_name']
+
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, idx: int):
+        sample = self.df.iloc[idx]
+        
+        sample = {'idx': idx,
+                  'ru_name': sample['ru_name'],
+                  'eng_name': sample['eng_name'],
+                  'label': float(sample['answer'])}
+        
+        if self.transform:
+            sample = self.transform(sample)
+        
+        return sample
+    
+class PaddingCollateFn:    
+    def __call__(self, batch):
+        
+        def pad_batch_with_zeros(l):
+            max_len = max([len(i) for i in l])
+            batch_size = len(l)
+            sample_size = l[0].size()[1]
+                
+            out = torch.zeros((batch_size, max_len, sample_size))
+                
+            for i, t in enumerate(l):
+                out[i, :l[i].size()[0], :] = l[i]
+                
+            return out
+        
+        ru_names = [item['ru_name'] for item in batch]
+        en_names = [item['eng_name'] for item in batch]
+        labels = [item['label'] for item in batch]
+        
+        ru_names = pad_batch_with_zeros(ru_names)
+        en_names = pad_batch_with_zeros(en_names)
+        labels = torch.tensor(labels, dtype=torch.float)
+        
+        items = {'ru_name': ru_names,
+                  'eng_name': en_names,
+                  'label': labels}
+
+        return [[ru_names, en_names], labels]
+    
