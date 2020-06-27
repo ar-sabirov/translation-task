7a7418bb109f004a873fda5895dfd75c13c5ea43
Arthur Sabirov <ar_sabirov@MacBook-Pro.local>
2020-06-27 16:27:08 +0300

    fix imports

--- src/models.py
+++ src/models.py
@@ -1,36 +1,37 @@
-import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import cat, sigmoid
 
-
-class RNN(torch.nn.Module):
+class RNN(nn.Module):
     def __init__(self,
                  input_size: int,
                  rnn_hidden_size: int,
                  rnn_num_layers: int,
                  fc_size: int,
                  output_size: int):
-        torch.nn.Module.__init__(self)
+        nn.Module.__init__(self)
 
-        self.rnn1 = torch.nn.LSTM(input_size,
+        self.rnn1 = nn.LSTM(input_size,
                                   rnn_hidden_size,
                                   rnn_num_layers,
                                   # bidirectional=True,
                                   batch_first=True)
 
-        self.rnn2 = torch.nn.LSTM(input_size,
+        self.rnn2 = nn.LSTM(input_size,
                                   rnn_hidden_size,
                                   rnn_num_layers,
                                   # bidirectional=True,
                                   batch_first=True)
 
-        self.linear1 = torch.nn.Linear(2 * rnn_hidden_size, fc_size)
-        self.linear2 = torch.nn.Linear(fc_size, output_size)
+        self.linear1 = nn.Linear(2 * rnn_hidden_size, fc_size)
+        self.linear2 = nn.Linear(fc_size, output_size)
 
     def forward(self, x):
         ru, en = x
         _, (ht1, _) = self.rnn1(ru)
         _, (ht2, _) = self.rnn2(en)
 
-        grus_out = torch.cat([ht1[-1], ht2[-1]], dim=1)
+        grus_out = cat([ht1[-1], ht2[-1]], dim=1)
 
         linear1 = self.linear1(grus_out)
         linear2 = self.linear2(linear1)
@@ -40,59 +41,59 @@ class RNN(torch.nn.Module):
         return output
 
 
-class RNNCNN(torch.nn.Module):
+class RNNCNN(nn.Module):
     def __init__(self,
                  rnn_input_size: int = 71,
                  rnn_hidden_size: int = 64,
                  rnn_num_layers: int = 1):
-        torch.nn.Module.__init__(self)
+        nn.Module.__init__(self)
         
-        # self.rnn = torch.nn.LSTM(rnn_input_size,
+        # self.rnn = nn.LSTM(rnn_input_size,
         #                          rnn_hidden_size,
         #                          rnn_num_layers,
         #                          batch_first=True)
 
-        self.conv1 = torch.nn.Conv2d(in_channels=1,
+        self.conv1 = nn.Conv2d(in_channels=1,
                                      out_channels=32,
                                      kernel_size=5,
                                      stride=2,
                                      padding=2)
 
-        self.pool = torch.nn.MaxPool2d(2)
+        self.pool = nn.MaxPool2d(2)
 
-        self.conv2 = torch.nn.Conv2d(in_channels=32,
+        self.conv2 = nn.Conv2d(in_channels=32,
                                      out_channels=64,
                                      kernel_size=5,
                                      stride=2,
                                      padding=2)
 
-        self.flatten = torch.nn.Flatten()
+        self.flatten = nn.Flatten()
 
     def forward(self, x):
         #x, (h_n, c_n) = self.rnn(x)
         x = x[:, None, :, :]
 
         x = self.conv1(x)
-        x = torch.relu(x)
+        x = F.relu(x)
         x = self.pool(x)
         
         x = self.conv2(x)
-        x = torch.relu(x)
+        x = F.relu(x)
         x = self.pool(x)
 
         return self.flatten(x)
 
 
-class CompareModel(torch.nn.Module):
+class CompareModel(nn.Module):
     def __init__(self):
-        torch.nn.Module.__init__(self)
+        nn.Module.__init__(self)
 
         self.rnn_cnn1 = RNNCNN()
         self.rnn_cnn2 = RNNCNN()
         
 
-        self.linear1 = torch.nn.Linear(4096, 128)
-        self.linear2 = torch.nn.Linear(128, 1)
+        self.linear1 = nn.Linear(4096, 128)
+        self.linear2 = nn.Linear(128, 1)
 
     def forward(self, x):
         ru, en = x
@@ -100,65 +101,65 @@ class CompareModel(torch.nn.Module):
         x_ru = self.rnn_cnn1(ru)
         x_en = self.rnn_cnn2(en)
         
-        x = torch.cat([x_ru, x_en], dim=1)
+        x = cat([x_ru, x_en], dim=1)
         
         x = self.linear1(x)
-        x = torch.relu(x)
+        x = F.relu(x)
         x = self.linear2(x)
         
-        output = torch.sigmoid(x)
+        output = sigmoid(x)
 
         return output
     
-class CNN(torch.nn.Module):
+class CNN(nn.Module):
     def __init__(self):
-        torch.nn.Module.__init__(self)
-        self.model = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=1,
+        nn.Module.__init__(self)
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=1,
                                 out_channels=128,
                                 kernel_size=7,
                                 padding=3),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.MaxPool2d(3),
-            torch.nn.Conv2d(in_channels=128,
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3),
+            nn.Conv2d(in_channels=128,
                             out_channels=128,
                                 kernel_size=7,
                                 padding=3),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.MaxPool2d(3),
-            torch.nn.Conv2d(in_channels=128,
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3),
+            nn.Conv2d(in_channels=128,
                                 out_channels=128,
                                 kernel_size=3,
                                 padding=1),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.Conv2d(in_channels=128,
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128,
                                 out_channels=128,
                                 kernel_size=3,
                                 padding=1),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.Conv2d(in_channels=128,
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128,
                                 out_channels=128,
                                 kernel_size=3,
                                 padding=1),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.MaxPool2d(3),
-            torch.nn.Flatten(),
-            torch.nn.Linear(1024, 1024),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.Linear(1024, 1024)
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3),
+            nn.Flatten(),
+            nn.Linear(1024, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 1024)
         )
         
     def forward(self, x):
         return self.model(x)
 
 
-class ChinatownModel(torch.nn.Module):
+class ChinatownModel(nn.Module):
     def __init__(self):
-        torch.nn.Module.__init__(self)
+        nn.Module.__init__(self)
         self.cnn1 = CNN()
         self.cnn2 = CNN()
         
-        self.linear = torch.nn.Linear(2048, 1)
+        self.linear = nn.Linear(2048, 1)
         
     def forward(self, x):
         x_ru, x_en = x
@@ -169,24 +170,24 @@ class ChinatownModel(torch.nn.Module):
         x_en = x_en[:, None, :, :]
         x_en = self.cnn2(x_en)
         
-        x = torch.cat([x_ru, x_en], dim=1)
-        x = torch.relu(x)
+        x = cat([x_ru, x_en], dim=1)
+        x = F.relu(x)
 
         x = self.linear(x)        
         
-        return torch.sigmoid(x)
+        return sigmoid(x)
         
         
 
-# class Net(torch.nn.Module):
+# class Net(nn.Module):
 #     def __init__(self):
 #         super(Net, self).__init__()
-#         self.conv1 = torch.nn.Conv2d(3, 6, 5)
-#         self.pool = torch.nn.MaxPool2d(2, 2)
-#         self.conv2 = torch.nn.Conv2d(6, 16, 5)
-#         self.fc1 = torch.nn.Linear(16 * 5 * 5, 120)
-#         self.fc2 = torch.nn.Linear(120, 84)
-#         self.fc3 = torch.nn.Linear(84, 10)
+#         self.conv1 = nn.Conv2d(3, 6, 5)
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.conv2 = nn.Conv2d(6, 16, 5)
+#         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+#         self.fc2 = nn.Linear(120, 84)
+#         self.fc3 = nn.Linear(84, 10)
 
 #     def forward(self, x):
 #         x = self.pool(F.relu(self.conv1(x)))
