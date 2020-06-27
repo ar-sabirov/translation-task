d61923f7ff0ac8703f2efc1fe6765a326a42876e
Arthur Sabirov <ar_sabirov@MacBook-Pro.local>
2020-06-27 18:13:18 +0300

    distr main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pytorch_lightning.callbacks import EarlyStopping
 from torch.optim.lr_scheduler import StepLR
 
 from src.dataset import PaddingCollateFn
-from src.models import RNN, CompareModel, ChinatownModel
+from src.torch.models.cnn import ChinatownModel
 from src.torch.lightning import LightningSystem
 from src.transforms import Lower, OneHotCharacters, Tokenize
 
@@ -31,27 +31,29 @@ if __name__ == "__main__":
 
     optimizer, optimizer_args = torch.optim.SGD, {'lr': 0.01, 'momentum': 0.9}
 
-    scheduler, scheduler_args = StepLR, {'step_size': 3, 'gamma': 0.5}
+    scheduler, scheduler_args = StepLR, {'step_size': 5, 'gamma': 0.5}
 
     system = LightningSystem(model=model,
-                             train_data_path='/Users/ar_sabirov/2-Data/kontur_test/train_subs.tsv',
-                             val_data_path='/Users/ar_sabirov/2-Data/kontur_test/val_subs.tsv',
+                             train_data_path='/root/train_subs.tsv',
+                             val_data_path='/root/val_subs.tsv',
                              loss=loss,
                              optimizer=optimizer,
                              optimizer_args=optimizer_args,
                              scheduler=scheduler,
                              scheduler_args=scheduler_args,
-                             num_workers=0,
+                             num_workers=1,
                              transforms=transforms,
                              num_classes=2,
-                             batch_size=16,
+                             batch_size=128,
                              collate_fn=PaddingCollateFn())
 
-    trainer = Trainer(  # log_save_interval=10,
-        val_check_interval=10,
-        limit_val_batches=0.1,
+    trainer = Trainer(
+        log_save_interval=1000,
+        row_log_interval=1000,
+        val_check_interval=1000,
+        #limit_val_batches=0.1,
         # distributed_backend='ddp',
-        # gpus=[0],
+        #gpus=1,
         fast_dev_run=True,
         # early_stop_callback=early_stop_callback,
         # precision=16,
